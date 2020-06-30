6e948f72ad28c1e85e9f8dc11c22cda686b113f0
Zora <zora@program.com.tw>
2020-06-30 15:26:24 +0800

    [feat] 新增側邊攔按鈕

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,7 +2,18 @@ import React from 'react'
 import '../style/SideBar.scss'
 
 const SideBar = () => {
-  return <div className="side-bar"></div>
+  return (
+    <div className="side-bar">
+      <ul>
+        <li>
+          <img src={require('../img/home.svg')} alt="首頁" />
+        </li>
+        <li>
+          <img src={require('../img/setting.svg')} alt="設定" />
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default SideBar
